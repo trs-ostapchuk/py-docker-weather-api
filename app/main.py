6f6c7babbbfb75dc638bc19713f951daa55aa719
Taras <tarasostapchuk27@gmail.com>
2026-01-27 20:51:10 +0100

    Create method get_weather()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "http://api.weatherapi.com/v1/current.json?key=15ac9e69e52c4ff0921192849262701&q=Paris&aqi=no"
+    response = requests.get(url)
+    data = response.json()
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    localtime = data["location"]["localtime"]
+    temp_c = data["current"]["temp_c"]
+    temp_condition = data["current"]["condition"]["text"]
+    print(f"Performing request to Weather API for city {city}...")
+    print(f"{city}/{country} {localtime} Weather: {temp_c} Celsius, {temp_condition}")
 
 
 if __name__ == "__main__":
